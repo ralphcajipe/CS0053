290e60978e7eb5e084f86940d48c36c8f9c16920
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2022-10-20 12:08:45 +0800

    Add TFA 5-1

---
+++ TechnicalFormative51.java
@@ -0,0 +1,30 @@
+package technicalformative51;
+
+/**
+ *
+ * @author 201911234
+ * Ralph Cajipe
+ */
+
+import java.util.Random;
+
+public class TechnicalFormative51 {
+    public static void main(String[] args) {
+        int[][] num = new int[5000][10000];
+        Random r = new Random();
+        int sum  = 0;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                num[i][j] = r.nextInt(100);
+                sum += num[i][j];
+            }
+            System.out.println("Sum of the row " + i + " is: " + sum);
+        }
+        long end = System.currentTimeMillis();
+        long totaltime = end - start;
+        
+        System.out.println("Sum of all rows: " + sum);
+        System.out.println("Total time: " + totaltime + "ms");
+    }
+}
